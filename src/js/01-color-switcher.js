e95d9d4e5ec72a1c91c69cb7068a7983b81de683
Kudese <d.kuprichenkov@gmail.com>
2022-11-08 03:42:23 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const elStartButton = document.querySelector('[data-start]');
+const elStopButton = document.querySelector('[data-stop]');
+const elBody = document.querySelector('body');
 
+console.log(elBody);
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+elStartButton.addEventListener('click', startChanhColor);
+elStopButton.addEventListener('click', stopChanhColor);
+let funSetinter;
+function startChanhColor() {
+  funSetinter = setInterval(() => {
+    elBody.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function stopChanhColor() {
+  clearInterval(funSetinter);
+}

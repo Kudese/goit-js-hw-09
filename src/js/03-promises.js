e2fd2724425795db0ba1d0fb3e2e167ff3e4973b
Kudese <d.kuprichenkov@gmail.com>
2022-11-10 01:19:49 +0200

    bagt3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,15 @@
+const elSbmitForm = document.querySelector('[type="submit"]');
+const elDelayInput = document.querySelector('[name="delay"]');
+const elStepInput = document.querySelector('[name="step"]');
+const elAmountInput = document.querySelector('[name="amount"]');
+
 function createPromise(position, delay) {
+  const p = new Promise((resolve, reject)=>{
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    
   } else {
     // Reject
-  }
+  }}
 }

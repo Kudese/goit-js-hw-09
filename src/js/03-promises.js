51bdd19e707bc1f8828193abdd6a3521316c6f1f
Kudese <d.kuprichenkov@gmail.com>
2022-11-10 02:58:27 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,15 +1,37 @@
 const elSbmitForm = document.querySelector('[type="submit"]');
-const elDelayInput = document.querySelector('[name="delay"]');
-const elStepInput = document.querySelector('[name="step"]');
-const elAmountInput = document.querySelector('[name="amount"]');
+const elForma = document.querySelector('.form');
+const elDelayInput = elForma.querySelector('[name="delay"]');
+const elStepInput = elForma.querySelector('[name="step"]');
+const elAmountInput = elForma.querySelector('[name="amount"]');
 
 function createPromise(position, delay) {
-  const p = new Promise((resolve, reject)=>{
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    
-  } else {
-    // Reject
-  }}
+  const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+ return p
+}
+
+elForma.addEventListener('submit', startPromis);
+
+function startPromis(e) {
+  e.preventDefault();
+  let delay =Number(elDelayInput.value);
+  let step = Number(elStepInput.value);
+  let amount = Number(elAmountInput.value);
+  for (let i = 0; i < amount; i++) {
+    createPromise(1 + i, delay + i * step)
+      .then(({position, delay})=>{
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      })
+      .catch((({position, delay}) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`)
+      }));
+  }
 }
